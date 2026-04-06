dbf4e7df2168b9b8a7d991fbfd0f2ab8a32badb2
Your Name <brightsy04@gmail.com>
2026-04-05 21:35:17 -0700

    2

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
-#include <iostream>
 #include <chrono>
-using namespace std;
 
-#include <iostream>
-using namespace std;
+auto start = chrono::high_resolution_clock::now();
 
-int main() {
-    cout << "Program works!" << endl;
-    return 0;
+vector<string> v;
+for (auto &s : codes) {
+    v.push_back(s);
 }
 
+auto end = chrono::high_resolution_clock::now();
+auto v_read = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+
+cout << "Vector Read: " << v_read << endl;
 /* syntax examples:
 auto start = high_resolution_clock::now()
 auto end = high_resolution_clock::now()
